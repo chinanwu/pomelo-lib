13f178dac4e4d16a813e5b24486edbfd7646c1b6
chinanwu <cawudev@gmail.com>
2020-09-30 17:35:14 -0700

    v: Include the... rest of the lib lol

---
+++ vue/README.md
@@ -0,0 +1,10 @@
+# Vue
+Part of the Salmon's Components franchise! This deals exclusively in Vue components.
+
+## Available Components
+Card 
+
+## RIUTFOHTDT 
+(Resources I Used to Figure Out How To Do This)
+
+- [Create a Vue.js component library](https://itnext.io/create-a-vue-js-component-library-as-a-module-part-1-a1116e632751)
---
+++ vue/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <Card>
+    <template v-slot:front>
+      <h1>Front of the card</h1>
+    </template>
+    <template v-slot:back>
+      <h1>Back of the card</h1>
+    </template>
+  </Card>
+</template>
+
+<script>
+import Card from "@/components/Card";
+
+export default {
+  name: "App",
+  components: {
+    Card
+  }
+};
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
