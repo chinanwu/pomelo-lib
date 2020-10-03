85174617e9be54f2ea5eaec211d06d00ed4c63d9
chinanwu <cawudev@gmail.com>
2020-10-02 22:58:57 -0700

    v: Add buttons to export list, delete old button component

--- vue/src/components/index.js
+++ vue/src/components/index.js
@@ -1,2 +1,7 @@
 export { default as Card } from "./Card";
-export { default as Button } from "./Button";
+
+// Buttons
+export { default as PButton } from "./Buttons/PButton";
+export { default as PLButton } from "./Buttons/PLButton";
+export { default as PMButton } from "./Buttons/PMButton";
+export { default as PSButton } from "./Buttons/PSButton";
